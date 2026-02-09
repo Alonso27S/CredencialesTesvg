ed65a250832649aa8a6320ee5804a5387f5a37d1
Jose Alonso Suarez <josealonso27sj@gmail.com>
2026-02-09 11:21:20 -0600

    Se modifico ruta del bac

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -49,7 +49,7 @@ const Login = ({ onLoginSuccess, onRequireToken }) => {
 
     try {
       /* ---- Petición al backend ---- */
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://credencialestesvg.com.mx/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
