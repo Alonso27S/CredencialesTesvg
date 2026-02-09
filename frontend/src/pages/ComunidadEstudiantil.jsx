9705f6329019932450585dce7643f666dcf7d197
Jose Alonso Suarez <josealonso27sj@gmail.com>
2026-02-09 15:29:30 -0600

    Se modifico URL de apis importados

--- frontend/src/pages/ComunidadEstudiantil.jsx
+++ frontend/src/pages/ComunidadEstudiantil.jsx
@@ -48,7 +48,7 @@ const importarExcel = async (e) => {
   formData.append("file", file);
 
   // 🔧 URL corregida
-  await fetch(`${API}/api/importacion/importar-excel`, {
+  await fetch(`${API}/api/importar-excel`, {
     method: "POST",
     body: formData,
   });
@@ -64,7 +64,7 @@ const importarExcel = async (e) => {
 const eliminar = async (id) => {
 
   // 🔧 URL corregida
-  await fetch(`${API}/api/importacion/importados/${id}`, {
+  await fetch(`${API}/api/importados/${id}`, {
     method: "DELETE",
   });
 
