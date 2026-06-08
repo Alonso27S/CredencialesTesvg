6a4df75454b4de92ad1cf33c896c23c961c4f06a
Alonso Suarez <josealonso27sj@gmail.com>
2026-06-08 12:38:36 -0600

    se cambio ruta de verificar

--- backend/routes/verificar.routes.js
+++ backend/routes/verificar.routes.js
@@ -3,6 +3,6 @@ import { verificarQR } from "../controllers/verificar.controller.js";
 
 const router = express.Router();
 
-router.post("/verificar", verificarQR);
+router.post("/verificar-credencial", verificarQR);
 
 export default router;
